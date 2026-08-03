7a7551b1f558d51640961b55810c1d886e8111f7
mohanracharla46 <mohanracharla46@gmail.com>
2026-08-03 20:31:38 +0530

    Fix Vercel production 404 by adding WSGI path correction middleware

--- api/index.py
+++ api/index.py
@@ -5,3 +5,19 @@ import sys
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
 from run import app
+
+# Vercel path correction WSGI middleware
+class VercelWSGIMiddleware:
+    def __init__(self, wsgi_app):
+        self.wsgi_app = wsgi_app
+
+    def __call__(self, environ, start_response):
+        path_info = environ.get('PATH_INFO', '')
+        if path_info == '/api/index':
+            environ['PATH_INFO'] = '/'
+        elif path_info.startswith('/api/index/'):
+            environ['PATH_INFO'] = path_info[10:]
+        return self.wsgi_app(environ, start_response)
+
+app.wsgi_app = VercelWSGIMiddleware(app.wsgi_app)
+
